7df81c81837d06d202158b1013bccf3428a36a45
Nice3point <nice3point@gmail.com>
2026-02-02 13:16:57 +0300

    [Git] Revert merge conflicts

--- build/Modules/PublishNugetModule.cs
+++ build/Modules/PublishNugetModule.cs
@@ -6,7 +6,6 @@ using ModularPipelines.Context;
 using ModularPipelines.DotNet.Extensions;
 using ModularPipelines.DotNet.Options;
 using ModularPipelines.Git.Extensions;
-using ModularPipelines.Models;
 using ModularPipelines.Modules;
 using Shouldly;
 
@@ -15,24 +14,23 @@ namespace Build.Modules;
 /// <summary>
 ///     Publish the NuGet packages to NuGet.org.
 /// </summary>
-[DependsOn<PackTemplatesModule>]
-[DependsOn<PackSdkModule>]
-[DependsOn<PublishGithubModule>]
-public sealed class PublishNugetModule(IOptions<BuildOptions> buildOptions, IOptions<NuGetOptions> nuGetOptions) : Module<CommandResult[]?>
+[DependsOn<PackTemplatesModule>(Optional = true)]
+[DependsOn<PackSdkModule>(Optional = true)]
+public sealed class PublishNugetModule(IOptions<BuildOptions> buildOptions, IOptions<NuGetOptions> nuGetOptions) : Module
 {
-    protected override async Task<CommandResult[]?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    protected override async Task ExecuteModuleAsync(IModuleContext context, CancellationToken cancellationToken)
     {
         var outputFolder = context.Git().RootDirectory.GetFolder(buildOptions.Value.OutputDirectory);
         var targetPackages = outputFolder.GetFiles(file => file.Extension == ".nupkg").ToArray();
         targetPackages.ShouldNotBeEmpty("No NuGet packages were found to publish");
 
-        return await targetPackages
-            .SelectAsync(async file => await context.DotNet().Nuget.Push(new DotNetNugetPushOptions
+        await targetPackages
+            .ForEachAsync(async file => await context.DotNet().Nuget.Push(new DotNetNugetPushOptions
                 {
                     Path = file,
                     ApiKey = nuGetOptions.Value.ApiKey,
                     Source = nuGetOptions.Value.Source
-                }, cancellationToken),
+                }, cancellationToken: cancellationToken),
                 cancellationToken)
             .ProcessOneAtATime();
     }
